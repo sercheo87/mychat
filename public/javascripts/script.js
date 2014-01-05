45af42575f84886f6ba480d5b18309160a403dd5
Sergio <chancaysergio@gmail.com>
2014-01-04 22:41:55 -0800

    restaurar componentes

--- public/javascripts/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-  $('#alerts').css('visibility','hidden');
-
-  $("#Send").click(function() {
-    sendMessage($('#Message').val());
-    $('#Message').val('');
-  });
-  
-});
